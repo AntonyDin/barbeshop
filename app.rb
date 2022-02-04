ab7a27fe8c09e2b522e33d9d153ee5bf79c364c9
AntonyDin <antdinyakov@gmaol.com>
2022-02-04 16:49:27 +0500

    getting parameters for post query

--- app.rb
+++ app.rb
@@ -16,12 +16,39 @@ get '/visit' do
 end
 
 post '/visit' do
-		# user_name, phone, date_time
-		@username = params[:username]
-		@date_time = params[:date_time]
+	# user_name, phone, date_time
+	@username = params[:username]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+		
+	@title = "Thank you!"
+	@message = "Уважаемый #{@username}, мы ждём вас #{@date_time}, ваш барбер #{@barber}"
 	  
-		@title = "Thank you!"
-		@message = "Уважаемый #{@username}, мы ждём вас #{@date_time}"
-	  
-		erb :visit
-	  end
+	f = File.open 'users.txt', 'a'
+  	f.write "User: #{@username}, date and time: #{@date_time}, barber: #{@barber}"
+  	f.close
+
+	erb :visit
+end
+
+=begin
+get '/cont' do
+  erb :cont
+end
+
+
+post '/cont' do
+	
+	@email = params[:email]
+	@floatingTextarea = params[:floatingTextarea]
+  
+    @otvet = "#{@email}, мы свяжемся с вами"
+
+	f = File.open 'contacts.txt', 'a'
+	  f.write "Ответить: #{@email}"
+	  f.close
+
+	erb :cont
+  end
+
+=end
