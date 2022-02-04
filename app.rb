3a2583f322b09364ccef618a011e224d3b2ff75d
AntonyDin <antdinyakov@gmaol.com>
2022-02-04 09:56:08 +0500

    changes to visit and app.rb

--- app.rb
+++ app.rb
@@ -14,3 +14,14 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+		# user_name, phone, date_time
+		@username = params[:username]
+		@date_time = params[:date_time]
+	  
+		@title = "Thank you!"
+		@message = "Уважаемый #{@username}, мы ждём вас #{@date_time}"
+	  
+		erb :visit
+	  end
