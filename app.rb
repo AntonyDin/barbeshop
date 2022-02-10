52c0d54ad82d5fcab4c88a27e83414a1413bf740
AntonyDin <antdinyakov@gmaol.com>
2022-02-10 14:36:49 +0500

    add db sql/visit and /cont

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
 
 get '/' do
   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -16,43 +18,53 @@ get '/visit' do
 end
 
 post '/visit' do
-	# user_name, phone, date_time
+  # подключаем базу данных SQL
+  db = SQLite3::Database.new 'users.sqlite'
+
+#user_name, phone, date_time
   @username = params[:username]
   @date_time = params[:date_time]
   @barber = params[:barber]
   @color = params[:color]
-	# хеш для вывода ошибки
+
+	# Валидация / хеш для вывода ошибки
   hh = {
 	:username => 'Введите имя',
 	:date_time => 'Введите дату и время'}
-	# для каждой пары ключ-значение
+	
+  # для каждой пары ключ-значение
   hh.each do |key, value|
+
 	#если параметр пуст
-	if params[key] == ''
+	  if params[key] == ''
 
 	#еременной error рисвоить value из хеша 
 	#а value из hh это сообщение об ощибке
-	#т.е. переменной error рисвоить сообщение об оштбке
-	@error = hh[key]
-	# вернуть представление
-		return erb :visit
-	end
-  end
+	#т.е. переменной error рисвоить сообщение об ошибке
 
-  if @username == ''
-	@error = 'Введите имя'
-	return erb :visit
+	@error = hh[key]
+
+	# вернуть представление
+		  return erb :visit
+	  end
   end
 
   @title = "Thank you!"
   @message = "Уважаемый #{@username}, мы ждём вас #{@date_time}, ваш барбер #{@barber}, цвет: #{@color}"
-
+=begin
   f = File.open 'users.txt', 'a'
   f.write "User: #{@username}, date and time: #{@date_time}, barber: #{@barber}, color: #{@color}"
   f.close
+=end
+  # добавим новую информацию в базу данных SQLite
+  db.execute "INSERT INTO Users (Username, Date_time, Barber, Color) VALUES ('#{@username}', '#{@date_time}', '#{@barber}', '#{@color}')"
+
+  db.close
 
   erb :visit
+
 end
+
 
 # contacts
 get '/cont' do
@@ -62,21 +74,47 @@ end
 
 post '/cont' do
 
+  db = SQLite3::Database.new 'cont.sqlite'
+
   @email = params[:email]
 
   # валидаци без хеша
 
   if @email == ''
-	@error = 'Введите @mail'
-	return erb :cont
+	  @error = 'Введите @mail'
+	  return erb :cont
   end
-
-
+  
   @answer = "#{@email}, мы свяжемся с вами"
 
-  f = File.open 'contacts.txt', 'a'
-  f.write "Ответить: #{@email}"
-  f.close
+  db.execute "INSERT INTO Contacts (Email) VALUES ('#{@email}')"
+
+  db.close
 
   erb :cont
 end
+
+
+
+
+
+
+ 
+=begin --- !? --- отправка почтовых сооющений --- !? ---
+  
+rescue => exception
+  
+end  Pony.mail(
+    :to => "dinyakov.a@yandex.ru",
+    :from => "dinyakov.a@yandex.ru",
+
+    :via => :smtp,
+    :via_options => { 
+      :address              => 'smtp.yandex.ru', 
+      :port                 => '25', 
+      :enable_starttls_auto => true, 
+      :user_name            => 'Антон Диняков', 
+      :password             => '55244255896321', 
+      :authentication       => :plain
+    })
+=end
